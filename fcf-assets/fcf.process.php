87f5785419f18b4a59a075a39761fd5f242d563d
ldwfalcon <66138904+ldwfalcon@users.noreply.github.com>
2021-11-26 13:16:57 -0800

    Delete fcf.process.php

--- fcf-assets/fcf.process.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ************************************
-// This file is part of a package from:
-// www.freecontactform.com
-// See license for details
-// ************************************
-
-include dirname(__FILE__).'/'.'fcf.config.php';
-include dirname(__FILE__).'/'.'process.php';
